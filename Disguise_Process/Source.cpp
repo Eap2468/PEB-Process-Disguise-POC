7f22c06310762363292414e90e5ffddf24fbcdd4
Eap2468 <68890963+Eap2468@users.noreply.github.com>
2023-10-11 17:58:58 -0400

    Update Source.cpp

--- Disguise_Process/Source.cpp
+++ Disguise_Process/Source.cpp
@@ -6,19 +6,19 @@ extern "C" _RTL_USER_PROCESS_PARAMETERS* GetParamInfo(void);
 
 int main()
 {
-	wchar_t newPath[] = L"C:\\windows\\system32\\notepad.exe";
-	wchar_t newDir[] = L"C:\\windows\\system32";
+	wchar_t newPath[] = L"C:\\windows\\system32\\notepad.exe"; //Path to replace current process path with
+	wchar_t newDir[] = L"C:\\windows\\system32";			   //Directory to replace current process with
 
-	_RTL_USER_PROCESS_PARAMETERS* params = GetParamInfo();
+	_RTL_USER_PROCESS_PARAMETERS* params = GetParamInfo(); //Get pointer to _RTL_USER_PROCESS_PARAMETERS and display the address for debugging
 	std::cout << "\\___ [ RTL_USER_PROCESS_PARAMETERS ]\n\t\\_0x" << std::hex << params << std::endl;
 	
-	params->ImagePathName.Buffer = newPath;
-	params->CommandLine.Buffer = newPath;
-	params->Reserved2[6] = newDir;
+	params->ImagePathName.Buffer = newPath; //ImagePathName buffer
+	params->CommandLine.Buffer = newPath;   //CommandLine buffer
+	params->Reserved2[6] = newDir;          //Process directory path
 	
 	std::cout << "PEB info should be changed";
 
-	while (!GetAsyncKeyState(VK_ESCAPE))
+	while (!GetAsyncKeyState(VK_ESCAPE)) //Keep process running to check results of POC
 	{
 		Sleep(500);
 	}
